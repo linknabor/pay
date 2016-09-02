06b5ed9e0cadf9d6130b3c73fbbd92ad892b7a24
davidhardson <davidhardson@hotmail.com>
2016-09-02 18:18:11 +0800

    common pay

---
+++ static/js/commonpay.js
@@ -0,0 +1,27 @@
+function addscript(jsUrl){
+	var head = document.getElementsByTagName('head')[0];
+	var script= document.createElement("script");
+    script.type = "text/javascript"; 
+    script.src=jsUrl; 
+    head.appendChild(script); 
+}
+
+function addcss(cssUrl){
+	var head = document.getElementsByTagName('head')[0];
+	var link = document.createElement("link");
+	link.setAttribute('rel','stylesheet');
+	link.href = cssUrl;
+    head.appendChild(link); 
+}
+
+function getWebRoot(){
+	var r = window.location+"";
+    var index = r.indexOf("/pay/");
+    return r.substr(0,index+1);
+}
+
+function getRefererParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
