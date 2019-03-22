b44fcf7c59fce06f8424d660e7d945923604069f
zhaoshiwei <861812342.@qq.com>
2019-03-22 16:32:26 +0800

    东湖支付

---
+++ ahzj3paymentdetail_static/baseurl.js
@@ -0,0 +1,4 @@
+window.baseurl = {
+    URL:'https://test.e-shequ.com/dhzj3/wechat/hexie/wechat/',     
+    wuye:'https://test.e-shequ.com/dhzj3/weixin/wuye/index1.html?state=123#/payEnquiry',
+}
---
+++ dhzj3orderpay_static/baseurl.js
@@ -0,0 +1,5 @@
+window.baseurl = {
+    URL:'https://test.e-shequ.com/dhzj3/wechat/hexie/wechat/',
+    gotoGroupDetail:'https://test.e-shequ.com/dhzj3/weixin/group/rgroupinvite.html?',//团购详情
+    home:'https://test.e-shequ.com/dhzj3/weixin/home/index.html?v=20160229',
+}
