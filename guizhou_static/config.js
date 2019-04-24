fc220864b75cf907a4168c81dfea0686e361b255
davidhardson <davidhardson@hotmail.com>
2019-04-24 19:35:35 +0800

    revert guizhou 2

--- guizhou_static/config.js
+++ guizhou_static/config.js
@@ -1,32 +1,21 @@
 var url=/127|test/.test(location.origin)?'https://test.e-shequ.com':
     /uat/.test(location.origin)?'https://uat.e-shequ.com':
     'https://www.e-shequ.com';
-var name='/baofang';
-var namepay='/pay';
+var name='/guizhou';
+var namepay='/guizhou/weixin';
 
 window.config = {
-     //团购里面用到baseurl
-     baseurl:url,
     URL:url+name+'/wechat/hexie/wechat/',
     footer:{
         wuye:url+name+'/weixin/wuye/index.html?v=20162299',
         person:url+name+'/weixin/person/index.html?v=20160229',
-        home:url+name+'/weixin/home/index.html?v=20160229',
+        home:url+name+'/weixin/home/index.html?v=20160229'
     },
     //person
-    gotoGroupDetail:url+name+'/weixin/group/rgroupinvite.html?',//团购
-   
+    gotoGroupDetail:url+name+'/weixin/group/rgroupinvite.html?',//团购详情
      //wuye支付成功
      wuye_zhifu:{
         url:url+name+'/weixin/wuye/index.html?state=123#/payEnquiry'
-    },
-    // 团购success页面用团购跳集市
-    group_onsalesindex:{
-        url:url+name+'/weixin/group/onsalesindex.html'
-    },
-    //团购success团购首页
-    rgrops_url :{
-        url:url+namepay+'/guizhourgroups.html?v=20160229'
     }
 
 
