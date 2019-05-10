b00314c2223bb5002cef0472358b9e843ac119c5
zhaoshiwei <861812342.@qq.com>
2019-05-10 15:05:35 +0800

    贵州生产上线测试

--- guizhou_static/config.js
+++ guizhou_static/config.js
@@ -1,33 +1,58 @@
 var url=/127|test/.test(location.origin)?'https://test.e-shequ.com':
     /uat/.test(location.origin)?'https://uat.e-shequ.com':
     'https://www.e-shequ.com';
-var name='/baofang';
+var name='/guizhou';
 var namepay='/pay';
-
-window.config = {
-     //团购里面用到baseurl
-     baseurl:url,
-    URL:url+name+'/wechat/hexie/wechat/',
+window.config={
+     
+    //公共的请求地址
+         URL:url+name+'/wechat/hexie/wechat/',
+    //公共的底部跳转
     footer:{
         wuye:url+name+'/weixin/wuye/index.html?v=20162299',
         person:url+name+'/weixin/person/index.html?v=20160229',
         home:url+name+'/weixin/home/index.html?v=20160229',
     },
-    //person
-    gotoGroupDetail:url+name+'/weixin/group/rgroupinvite.html?',//团购
-   
-     //wuye支付成功
-     wuye_zhifu:{
-        url:url+name+'/weixin/wuye/index.html?state=123#/payEnquiry'
+    //现金券
+    person_coupons:{
+        coupon:url+name+'/weixin/group/onsalesindex.html',
     },
-    // 团购success页面用团购跳集市
-    group_onsalesindex:{
-        url:url+name+'/weixin/group/onsalesindex.html'
+    //person跳转支付页
+    person_zhifu:{
+        orderpays:url+namepay+'/guizhouorderpay.html?state=123#/repairPay?',//跳转支付
+        comment:url+namepay+'/guizhouorderpay.html?state=123#/commentxiu',//跳转评论
+        order:url+namepay+'/guizhouorderpay.html?start=123#/',//全部订单
+        grouporders:url+namepay+'/guizhouorderpay.html?start=123#/grouporders',//团购订单
+        homeorders:url+namepay+'/guizhouorderpay.html?start=123#/homeorders',//服务订单
     },
-    //团购success团购首页
-    rgrops_url :{
-        url:url+namepay+'/guizhourgroups.html?v=20160229'
+    //报修
+    person_repair:{
+        url:url+name+'/weixin/wuye/index.html?state=123#/butler?category=2',//报修
+        fuwux:url+name+'/weixin/wuye/index.html?state=123#/butler?category=0',//服务需求 
+        butler:url+name+'/weixin/wuye/index.html?state=123#/butler?category=1',//建议
+    },
+     //物业绑定房子
+     house_domain: {
+        domain:/127|test/.test(location.origin)?'test.e-shequ.com':
+        /uat/.test(location.origin)?'at.e-shequ.com':
+        'www.e-shequ.com'
+    },
+    //物业支付跳转
+    wuye_payment:{
+        payment:url+namepay+'/guizhouwuyepay.html?#/?'//物业支付成功跳转
+    },
+
+    //保洁
+    home_baojie:{
+        url1:url+namepay+'/guizhoubaojiepay.html?state=123#/order',//支付订单
+        url2:url+namepay+'/guizhoubaojiepay.html?state=123#/'//立即预约
     }
+    
+
+    
 
 
+
+   
 }
+
